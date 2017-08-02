0284e8cc9956717e7000aeb5e551d1dfcbf58525
Boss-PC\Boss <nathanrobertsnz@gmail.com>
2017-08-02 23:49:25 +1200

    no message

---
+++ WebApplication2/WebApplication2/Models/EmailFormModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using WebApplication2.Models;
+
+namespace MVCEmail.Models
+{
+    public class EmailFormModel
+    {
+        [Required, Display(Name = "Your Name")]
+        public string FromName { get; set; }
+        [Required, Display(Name = "Your Email"), EmailAddress]
+        public string FromEmail { get; set; }
+
+
+       
+        [Required, Display(Name = "Your Phone Number")]
+        
+        public string FromPhoneNumber { get; set; }
+        
+        [Display(Name = "Message")]
+        public string Message { get; set; }
+        
+    }
+}
